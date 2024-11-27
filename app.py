90b5e72f0890a7de31388eec87d51cf5ee45eee3
MichaelRobb6 <michaelrobb6@gmail.com>
2024-11-27 18:56:11 -0500

    newcomimit

--- app.py
+++ app.py
@@ -123,4 +123,5 @@ if st.button("Submit"):
     formated_val = '{:,}'.format(torch.exp(prediction).item())
     # Example placeholder for model prediction or processing
     # Replace this with your actual model code
-    st.success(f'Your movie is expected to make ${formated_val}')
+    st.success('You suck mad butt and your idea for a New Girl movie would stink. I know that\'s what you wanted to make')
+    # st.success(f'Your movie is expected to make ${formated_val}')
