e8ec9d6aa3bd2340fa6acf0cfabed90e14cd01cf
MichaelRobb6 <michaelrobb6@gmail.com>
2024-11-27 18:56:48 -0500

    Update app.py

--- app.py
+++ app.py
@@ -118,9 +118,9 @@ if st.button("Submit"):
     # Simulate passing data to a model
     st.subheader("Submitted Data")
     st.write(movie_encoded)
-    st.write(torch.exp(prediction).item())
+    #st.write(torch.exp(prediction).item())
 
-    formated_val = '{:,}'.format(torch.exp(prediction).item())
+    #formated_val = '{:,}'.format(torch.exp(prediction).item())
     # Example placeholder for model prediction or processing
     # Replace this with your actual model code
     st.success('You suck mad butt and your idea for a New Girl movie would stink. I know that\'s what you wanted to make')
